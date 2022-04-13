54b191abc365beb515b3ffcb7e9f645de6ec7d7d
KiranVegeta1 <kiranvegeta.1@gmail.com>
2022-04-14 03:01:33 +0530

    Initial DCommit

---
+++ src/components/MarkdownMain.jsx
@@ -0,0 +1,13 @@
+import FolderUpload from "./folderUpload";
+import Navbar from "./navbar";
+
+const MarkdownMain = () => {
+    return (
+        <div>
+            <Navbar/>
+            <FolderUpload/>
+        </div>
+    )
+}
+
+export default MarkdownMain;
